175e8aa1408711264bb69993c53a90dbd0d94385
CoolhanD <abrawa@gmail.com>
2021-03-10 19:39:35 +0200

    fixed speed of snake

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
-const ground = new Image();
+const ground = new Image(); 
 const foodImg = new Image();
 
 ground.src = 'img/ground.png';
@@ -10,9 +10,9 @@ foodImg.src = 'img/food.png';
 
 let box = 32;
 let score = 0;
-let food = {
+let food = { 
     x: Math.floor((Math.random() * 17 + 1)) * box, 
-    y: Math.floor((Math.random() * 15 + 3)) * box,
+    y: Math.floor((Math.random() * 15 + 3)) * box, 
 };
 let snake = [];
 snake[0] = {
@@ -86,4 +86,4 @@ function drawGame() {
     snake.unshift(newHead);
 };
 
-let game = setInterval(drawGame, 100);
+let game = setInterval(drawGame, 150);
